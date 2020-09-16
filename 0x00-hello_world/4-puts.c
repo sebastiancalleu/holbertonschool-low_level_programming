9e913543f6e6f6d67b5d93eb60c58de46be06156
Sebastian <sebastian.calleu@gmail.com>
2020-09-16 12:22:18 -0500

    3thtry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
- 
+/**
+ * main - entry
+ *
+ * Return - finish
+ */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
