0a08395ff42eb24752679dbb9c0240295d2250ca
Sebastian <sebastian.calleu@gmail.com>
2020-09-17 17:10:22 -0500

    idktry2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  *
  * Return: always be 0
 */
-int main(void)     
-{ 
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-219\n", 59); 
-	return (1);   
-} 
-     
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-219\n", 59);
+return (1);
+}
