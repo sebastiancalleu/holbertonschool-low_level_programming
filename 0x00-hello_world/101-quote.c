fbb1fea932c44e80a87f595f9e1254bc3dce44d1
Sebastian <sebastian.calleu@gmail.com>
2020-09-17 17:08:35 -0500

    idktry

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry
  *
  * Return: always be 0
 */
-
-int main(void)
-{
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
-}
+int main(void)     
+{ 
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-219\n", 59); 
+	return (1);   
+} 
+     
