23c7be1893a14b07986829610b6910e76a11f87e
Sebastian <sebastian.calleu@gmail.com>
2020-09-22 18:34:31 -0500

    firsttry

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -this function display the fibo.
+ *
+ * return: is 0
+ */
+int main(void)
+{
+int c;
+long a, b, d;
+a = 1;
+b = 2;
+printf("%ld, ", a);
+for (c = 0; c <= 48; c++)
+{
+d = a + b;
+a = b;
+b = d;
+printf("%ld", a);
+if (c !=48)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
+}
