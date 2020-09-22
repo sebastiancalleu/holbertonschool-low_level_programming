a3360e723c78fc4651fd425f5e931f2f1cff140b
Sebastian <sebastian.calleu@gmail.com>
2020-09-22 18:55:06 -0500

    2ndtry

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,19 @@
 int main(void)
 {
 int c;
-long a, b, d, e;
+long a, b, d, e, f;
 a = 1;
 b = 2;
 e = 0;
 for (c = 0; c <= 48; c++)
 {
-d = a + b;
-a = b;
-b = d;
 if (a < 4000000)
 {
 e = e + a;
 }
+d = a + b;
+a = b;
+b = d;
 }
 printf("%ld", e);
 printf("\n");
