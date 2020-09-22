1fe44499273bb57f5fc85d8b2a2e5917413ba8ad
Sebastian <sebastian.calleu@gmail.com>
2020-09-22 00:41:28 -0500

    3thtry

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 /**
- * main - check if sign of the number
- *
- * Return: Always 0.
+ * print_sign - check if sign of the number
+ *@n: variable input
+ * Return: 1 if it is positive, 0 if it is zero and -1 if it is negative
  */
 int print_sign(int n)
 {
-if (n>0)
+if (n > 0)
 {
-_putchar('+'); 
-return 1;
+_putchar('+');
+return (1);
 }
 else
 {
 if (n == 0)
 {
 _putchar('0');
-return 0;
+return (0);
 }
 else
 {
 _putchar('-');
-return -1;
+return (-1);
 }
 }
 }
