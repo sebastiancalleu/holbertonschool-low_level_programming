7e9108dd751827f3463d5ec790013bbe88e197bb
Sebastian <sebastian.calleu@gmail.com>
2020-09-22 00:38:06 -0500

    3thtry

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * main - check if is lowercase.
- *
- * Return: Always 0.
+ * _isalpha - check if is a alphabetic character.
+ *@c: input variable
+ * Return: 1 if it is a alphabetic character.
  */
 int _isalpha(int c)
 {
-	if  ((c >= 'A' && c <='Z') || (c >= 'a' && c <= 'z'))
-return 1;
-return 0;
+if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+return (1);
+return (0);
 }
