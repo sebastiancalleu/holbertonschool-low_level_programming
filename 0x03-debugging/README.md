e5e4080ca15eaa24764aca4ec4ef2c1291302741
Sebastian <sebastian.calleu@gmail.com>
2020-09-22 13:41:36 -0500

    firsttry

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+# Debugging
+in this project we are going to study the debuggins methods.
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: always be 0
+ */
+void positive_or_negative(int i)
+{
+if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}
+}
