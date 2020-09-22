e62e2ba3cb540f766bc622c054e93e683181a06f
Sebastian <sebastian.calleu@gmail.com>
2020-09-22 00:29:13 -0500

    3thtry

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,13 @@
 #include "holberton.h"
 /**
- * entry - main, this is going to print the alphabet
- *
- * retunr: is 0 
+ * print_alphabet_x10 - this function prints ten times the alphabet
  */
 void print_alphabet_x10(void)
 {
-	int a, b;
-for(b = 1; b <= 10; b++)
+int a, b;
+for (b = 1; b <= 10; b++)
 {
-for(a = 'a'; a <= 'z'; a++)
+for (a = 'a'; a <= 'z'; a++)
 {
 _putchar(a);
 }
