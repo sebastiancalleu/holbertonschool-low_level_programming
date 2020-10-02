e31129b92e9fbc5ee9679a2e214ea181d402ae81
Sebastian <sebastian.calleu@gmail.com>
2020-10-01 21:19:04 -0500

    firsttry

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * root - transform a string to a rot13
+ * @s: input string.
+ * Return: the leet string.
+ */
+char *rot13(char *s)
+{
+int a, b;
+char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (b = 0; s[b]; b++)
+{
+for (a = 0; l[a]; a++)
+{
+if (s[b] == l[a])
+{
+s[b] = n[a];
+break;
+}
+}
+}
+return (s);
+}
