cc103375c35296463e297a4bd4dc4c3c4cc4c35e
Sebastian <sebastian.calleu@gmail.com>
2020-10-01 23:40:00 -0500

    firsttry

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * infinite_add - this function add an infinite number.
+ * @n1: number to add.
+ * @n2: number to add.
+ * @r: result number.
+ * @size_r: size of the result
+ * Return: this is the sum.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+return (r);
+}
