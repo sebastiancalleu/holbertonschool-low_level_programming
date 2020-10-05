ac964e16ed00153b441a7c093cdd5929e295bd12
Sebastian <sebastian.calleu@gmail.com>
2020-10-05 13:21:40 -0500

    secondtry

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n--)
-		*dest++ = *src++;
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
 	return (dest);
 }
