d43b4e9a51faf7c6df05fbfab6a3e129149c3bff
Sebastian <sebastian.calleu@gmail.com>
2020-10-07 19:41:36 -0500

    secondtry

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * wildcmp - this function compares two strings.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: 1 if match, 0 if doesnt match.
  */
 
 int wildcmp(char *s1, char *s2)
