099ded0689b8127f51ed23ede50aaf175d9d2d43
Sebastian <sebastian.calleu@gmail.com>
2020-10-13 15:03:36 -0500

    firsttry

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - this functions freed a grid.
+ * @grid: the grid.
+ * @height: the height of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a <= height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,3 +2,4 @@ char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int, int);
+void free_grid(int **, int);
