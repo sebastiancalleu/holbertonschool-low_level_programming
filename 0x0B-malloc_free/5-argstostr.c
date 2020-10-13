606030309900193e83553c036a09c1ea6449509a
Sebastian <sebastian.calleu@gmail.com>
2020-10-13 16:59:14 -0500

    idktry

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -9,22 +9,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int a;
+	int a, b, c, d, e, f;
 	char *str;
 
+	c = 0;
+	d = 0;
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	str = malloc(1000);
+	for (e = 0; e < ac; e++)
+	{
+		for (f = 0; av[e][f]; f++)
+		{
+			d = d + 1;
+		}
+	}
+	str = malloc((d + ac) * sizeof(char));
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 	for (a = 0; a < ac; a++)
 	{
-		strcat(str, av[a]);
-		strcat(str, "\n");
+		for (b = 0; av[a][b]; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
 	}
 	return (str);
 }
+
