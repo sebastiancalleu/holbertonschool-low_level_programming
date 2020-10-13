167b98a93c6a166e0455dea91fabfa05f82f5490
Sebastian <sebastian.calleu@gmail.com>
2020-10-13 13:39:08 -0500

    secondtry

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,14 @@ char *str_concat(char *s1, char *s2)
 	int a, b, c, e, f;
 	char *d;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (a = 0; s1[a]; a++)
 	{
 	}
