43e1ab9077ab210fdc963af5839f026edcac64e8
Sebastian <sebastian.calleu@gmail.com>
2020-10-13 14:24:06 -0500

    secondtry

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,6 +11,10 @@ int **alloc_grid(int width, int height)
 	int **array;
 	int a;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 	{
