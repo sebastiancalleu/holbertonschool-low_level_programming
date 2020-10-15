01ba457b1528800a468709a3688880c065c95011
Sebastian <sebastian.calleu@gmail.com>
2020-10-15 13:11:25 -0500

    firsttry

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,12 @@
+#include "holberton"
+#include <stdlib.h>
+/**
+ * main - this function multiplies two arguments.
+ * @argc: arg counter.
+ * @argv: arg string
+ * Return: is 0
+ */
+int main(int argc, char *argv[])
+{
+
+}
