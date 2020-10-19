8243038739cb8ee0b0922a0ab2a5a1555a6ef699
Sebastian <sebastian.calleu@gmail.com>
2020-10-19 16:05:21 -0500

    firsttry

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,15 @@
  * @age: age.
  * @owner: the dog owner's name.
  **/
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
+dog_t *new_dog(char *, float, char *);
 #endif
 
