0c47cf0b040d1a6dfb01b85a6ff9600d97745569
Sebastian <sebastian.calleu@gmail.com>
2020-10-21 12:10:29 -0500

    secondtry

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,8 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int a;
+	size_t a;
 
+	if (array && action)
 	for (a = 0; a < size; a++)
 	{
 		action(array[a]);
