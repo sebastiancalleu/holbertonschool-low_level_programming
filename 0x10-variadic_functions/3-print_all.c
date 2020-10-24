8eae5f5087b80e95a60e23d2ff66d1caeba8d03f
Sebastian <sebastian.calleu@gmail.com>
2020-10-24 12:47:06 -0500

    modifying headers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,3 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 /**
  * print_all - this function prints all.
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARFUNC_H
 #define VARFUNC_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int , ...);
