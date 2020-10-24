6b3594ec652fa3bff866b60f406d3c568ebc539f
Sebastian <sebastian.calleu@gmail.com>
2020-10-24 16:01:00 -0500

    deletes stdlib

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,6 @@
 #define VARFUNC_H
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
