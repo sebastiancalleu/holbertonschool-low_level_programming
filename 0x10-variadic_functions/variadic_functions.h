cae6ef75c7e2e07ce9e30c9a5935ff9207cd6e8b
Sebastian <sebastian.calleu@gmail.com>
2020-10-24 12:58:09 -0500

    adding putchar prot

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,5 @@ int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int , ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
 #endif
