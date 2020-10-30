e32d4a8891331f75592feab25c7babf909931cfa
Sebastian <sebastian.calleu@gmail.com>
2020-10-30 10:17:40 -0500

    adding _strlen function

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 /**
+ * _strlen - this function calculates the length.
+ * @str: the string.
+ * Return: the length.
+ */
+int _strlen(const char *str)
+{
+int a;
+for (a = 0; str[a] != 0; a++)
+{
+}
+return (a);
+}
+
+/**
  * add_node_end - function that adds a node at the end of the list.
  * @head: pointer to a pointer to a struct.
  * @str: the string to add to the node.
@@ -7,19 +21,14 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-char *s = strdup(str);
-unsigned int a;
 list_t *temp;
 list_t *newnode = (list_t *) malloc(sizeof(list_t));
 if (newnode == NULL)
 {
 	return (NULL);
 }
-for (a = 0; s[a]; a++)
-{
-}
-newnode->len = a;
-newnode->str = s;
+newnode->len = _strlen(str);
+newnode->str = strdup(str);
 newnode->next = NULL;
 if ((*head) == NULL)
 {
