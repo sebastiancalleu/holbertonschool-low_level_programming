d5b26383fee7f2dcf180b47c1be59dd3b2b4e400
Sebastian <sebastian.calleu@gmail.com>
2020-11-04 08:51:46 -0500

    adding edge case of 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->n = n;
 	tmp = *head;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	for (a = 0; tmp; a++)
 	{
-		if (a == idx)
+		if (a == (idx - 1))
 		{
 			new->next = tmp->next;
 			tmp->next = new;
