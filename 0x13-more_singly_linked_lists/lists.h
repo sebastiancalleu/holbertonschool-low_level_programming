5c06af696b91e457595d855c23425b18632bb6f9
Sebastian <sebastian.calleu@gmail.com>
2020-11-04 10:04:54 -0500

    fixing betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+
 
 /**
  * struct listint_s - singly linked list
@@ -14,15 +16,15 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+		int n;
+		struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **, const int);
-listint_t *add_nodeint_end(listint_t **, const int );
+listint_t *add_nodeint_end(listint_t **, const int);
 void free_listint(listint_t *);
 void free_listint2(listint_t **);
 int pop_listint(listint_t **);
