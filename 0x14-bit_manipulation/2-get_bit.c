d99d002f9146f16aa23b3e1a811a8b65883888d4
Sebastian <sebastian.calleu@gmail.com>
2020-11-05 16:56:34 -0500

    adding edge case if index is bigger than allowed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int srch = 0x01;
 
 	srch <<= index;
+	if (index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
 	if ((n & srch))
 		return (1);
 	else
