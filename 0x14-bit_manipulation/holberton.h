cb7b4a6fbdbb3fd1441c47d04859c5f685cf1854
Sebastian <sebastian.calleu@gmail.com>
2020-11-05 17:24:13 -0500

    adding proto to header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -6,4 +6,5 @@ void print_binary(unsigned long int);
 int _putchar(char);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
 #endif
