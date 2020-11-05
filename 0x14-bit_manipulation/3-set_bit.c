f60ce55186a0e2cd38070b8762983ac5bcc27482
Sebastian <sebastian.calleu@gmail.com>
2020-11-05 17:13:10 -0500

    fixing betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,6 +14,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	srch <<= index;
 	if (index > (sizeof(unsigned long int) * 8) - 1)
 		return (-1);
-	*n = *n|srch;
+	*n = *n | srch;
 	return (1);
 }
