aa1ebf81ca6408ae4fc394fad69e4ca190ef7f72
Sebastian <sebastian.calleu@gmail.com>
2020-11-05 17:23:02 -0500

    creating file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - this function set a bit of a number to 1.
+ * @n: the number.
+ * @index: the index.
+ * Return: the new number.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+		unsigned long int srch = 0x01;
+
+	srch <<= index;
+	if (index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+	*n = *n & ~srch;
+	return (1);
+}
