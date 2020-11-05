a2c93324545fada24d6104c7d90f3bd5db397433
Sebastian <sebastian.calleu@gmail.com>
2020-11-05 16:54:39 -0500

    creating file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_bit - this function get the value of a bit at a index
+ * @n: the bit.
+ * @index: the index
+ * Return: the bit.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int srch = 0x01;
+
+	srch <<= index;
+	if ((n & srch))
+		return (1);
+	else
+		return (0);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,5 +4,5 @@
 unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
 int _putchar(char);
-
+int get_bit(unsigned long int, unsigned int);
 #endif
