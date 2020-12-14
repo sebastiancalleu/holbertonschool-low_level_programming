935eddecf9d7e15752f373a252093ff20ad97f0f
sebastian <sebastian.calleu@gmail.com>
2020-12-13 22:59:29 -0500

    feat(NULL): adding NULL when index not found.

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - this function calculates the lengh of a list.
+ * @h: the list.
+ * Return: the number of elements.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int a = 0;
+
+	for (a = 0; h != NULL; a++)
+	{
+		h = h->next;
+	}
+	return (a);
+}
+
+/**
  * get_dnodeint_at_index - this function get a node at index.
  * @head: the list.
  * @index: the index.
@@ -10,7 +27,10 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int a;
-
+	if (index > (dlistint_len(head) - 1))
+	{
+		return(NULL);
+	}
 	for (a = 0; a < index; a++)
 	{
 		head = head->next;
