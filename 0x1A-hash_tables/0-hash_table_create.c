dbe3be98d21822d034d599ce04e0a720c510270c
sebastian <sebastian.calleu@gmail.com>
2021-01-15 11:41:49 -0500

    factor(ht, ht->array): adding NULL return.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,8 +14,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int a;
 
 	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		return (NULL);
+	}
 	for (a = 0; a < size; a++)
 	{
 		ht->array[a] = NULL;
