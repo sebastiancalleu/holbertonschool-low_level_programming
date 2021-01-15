ca889d5e9a4c44d45f06093758bca3ab12301a8f
sebastian <sebastian.calleu@gmail.com>
2021-01-15 16:10:25 -0500

    adding files

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - this function get value from a hash table.
+ * @ht: the hash table.
+ * @key: the key.
+ * Return: the value.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *cpnode;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+	{
+		return (NULL);
+	}
+	cpnode = ht->array[index];
+	while (cpnode)
+	{
+		if (strcmp(cpnode->key, key) == 0)
+			return (cpnode->value);
+	}
+	return (NULL);
+}
