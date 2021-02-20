d29cb341ff712e248c029b547f420ce9398a7c2e
sebastian <sebastian.calleu@gmail.com>
2021-02-19 23:39:45 -0500

    adding edge features

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module to find island perimeter """
 
+
 def island_perimeter(grid):
     """
     island_perimeter
@@ -11,12 +12,18 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
+                if grid[i][j - 1] == 0 or j == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if grid[i - 1][j] == 0 or i == 0:
                     perimeter += 1
-                if grid[i - 1][j] == 0:
+                try:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                except:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                try:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                except:
                     perimeter += 1
     return perimeter
