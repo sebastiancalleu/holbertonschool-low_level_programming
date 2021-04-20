148b47889e302fc662ac6a512659401a732e1ea3
sebastian <sebastian.calleu@gmail.com>
2021-04-20 12:46:45 -0500

    deleting break

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,10 +1,11 @@
 #include "search_algos.h"
+
 /**
  * linear_search - searching for a index in array
  * @array: the array
  * @size: the size of the array
  * @value: the value to find.
- * Return: the index
+ * Return: the index when is found
  */
 
 int linear_search(int *array, size_t size, int value)
@@ -15,12 +16,9 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (counter = 0; counter < size; counter++)
 	{
-		printf("Value checked array[%d] = [%d]", counter, array[counter]);
+		printf("Value checked array[%d] = [%d]\n", counter, array[counter]);
 		if (array[counter] == value)
-			break;
+			return(counter);
 	}
-	if ((counter == size) && (array[counter] != value))
-		return (-1);
-
-	return (counter);
+	return (-1);
 }
