7bbbf7d5e98647bafabe36419e629621f8ef85ae
sebastian <sebastian.calleu@gmail.com>
2021-04-20 21:07:23 -0500

    array[]

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,12 +12,12 @@ int linear_search(int *array, size_t size, int value)
 {
   size_t i = 0;
 
-  if (array == NULL)
+  if (!array)
     return (-1);
 
   for (i = 0; i < size; i++)
   {
-    printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+    printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
     if (value == *(array + i))
       return (i);
   }
