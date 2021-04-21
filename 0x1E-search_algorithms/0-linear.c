a4ed510b723950c982cf7fdbedd94d31740557bf
sebastian <sebastian.calleu@gmail.com>
2021-04-20 21:15:58 -0500

    no blank lines

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,13 +14,11 @@ int linear_search(int *array, size_t size, int value)
 
   if (!array)
     return (-1);
-
   for (i = 0; i < size; i++)
   {
     printf("Value checked array[%d] = [%d]\n", i, array[i]);
     if (array[i] == value)
       return (i);
   }
-
   return (-1);
 }
